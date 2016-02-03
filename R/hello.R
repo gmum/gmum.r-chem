52132e9aa536955fb2cb33a789678e33b71ca90b
osipam <manisiek@hotmail.com>
2016-02-03 10:13:40 +0100

    Revert "3.02"
    This reverts commit 202c9d2f731407918e608ea2c83cb1802cd42eaf.

--- R/hello.R
+++ R/hello.R
@@ -1,3 +1,18 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
 createObject <- function(a, b){
   
   newS3 <- list(
@@ -9,51 +24,66 @@ createObject <- function(a, b){
   return(newS3)
 }
 
-#tworzy macierz
+createObjectPoint <- function(x,y,z){
+  newObject <- list(
+    x_crd = x,
+    y_crd = y,
+    color = z
+  )
+  
+  class(newObject) <- append(class(newObject),"createObjectPoint")
+  return(newObject)
+}
+
 dataExample <- function(){
-  x <- c(2,5,1,7,-2,6,-7,-2,4,7,1,3,-3,5,8,2,8,1,9,0)
-  y <- c(1,-4,6,3,-4,6,-8,1,-2,7,-2,8,5,3,8,-4,6,-4,2,1)
-  data <- matrix(cbind(x,y),length(x),2)
+  x = c(2,5,1,7,-2,6,-7,-2,4,7,1,3,-3,5,8,2,8,1,9,0)
+  y = c(1,-4,6,3,-4,6,-8,1,-2,7,-2,8,5,3,8,-4,6,-4,2,1)
+  data = c(x,y)
   return(data)
 }
 
-#narazie zwraca tylko 2 z 4 wartosci
-summary <- function(data,vector){
-  red_c <- 0
-  blue_c <- 0
-  n <- length(data)/2
-  a <- vector$first_arg
-  b <- vector$second_arg
-  color <- "green"
-  for(i in 1:n){ # tak wiem, ze to kolorowanie jest tragiczne
-    value = a*data[i] + b
-    if(value > data[i,2]){color <- "red"}
-    else if(value <= data[i,2]){color <- "blue"}
-    if(color == "red"){red_c <- red_c + 1}
-    else if(color == "blue"){blue_c <- blue_c + 1}
+pointCoordinates <- function(data,vector){
+  n = length(data)/2
+  points = c()
+  for(i in 1:n){
+    value = vector$first_arg * data[i] + vector$second_arg
+    if(value > data[n+i]){
+      color = "red"
+    }
+    if(value <= data[n+i]){
+      color = "blue"
+    }
+    point <- createObjectPoint(data[i],data[n+i],color)
+    points = c(points, point)
   }
-  formula <- 0
-  time <- 0 #system.time(LR(data))
-  sum_object <- matrix(c(red_c,blue_c,time,formula), ncol<-1, nrow<-4, byrow = TRUE)
-  colnames(sum_object) <- c("RED", "BLUE", "TIME", "FORMULA")
-  rownames(sum_object) <- c("VALUE")
-  return(sum_object)
+  return(points)
 }
 
-#przyjmuje macierz
+printPoints <- function(data){
+  n = length(data)/2
+  x_arr = c()
+  y_arr = c()
+  for(i in 1:n){
+    x_arr = c(x_arr, data[1+(2*(i-1))])
+    y_arr = c(y_arr, data[2+(2*(i-1))])
+  }
+  df <- data.frame(x_arr, y_arr)
+  return (df)
+}
+
 LR <- function(data){
   n = length(data)/2
-  x <- data[,1]
-  y <- data[,2]
+  x = data[seq(1,n)]
+  y = data[seq(n+1,2*n)]
   sum_x = sum(x)
   sum_y = sum(y)
-  sum_xy = 0
-  x_square = 0
+  iloczyn = 0
+  kwadraty = 0
   for(i in 1:n){
-    sum_xy = sum_xy + x[i]*y[i]
-    x_square = x_square + (x[i])^2
+    iloczyn = iloczyn + x[i]*y[i]
+    kwadraty = kwadraty + (x[i])^2
   }
-  b = (n*sum_xy - sum_x*sum_y)/(n*x_square - (sum_x)^2)
+  b = (n*iloczyn - sum_x*sum_y)/(n*kwadraty - (sum_x)^2)
   y_d = sum_y/n
   x_d = sum_x/n
   a = y_d - b*x_d
@@ -61,13 +91,23 @@ LR <- function(data){
   return (result)
 }
 
-#powyrzucane niepotrzebne rzeczy, ale nie ma jeszcze kolorowania pkt
 mojPlot <-function(vector, data){
-  a <- vector$first_arg
-  b <- vector$second_arg
-  n <- length(data)/2
-  df <- data.frame(data[,1],data[,2])
-  plot(df, xlim = c(-10,10), ylim = c(-10,10), xlab ="x", ylab = "y", col = "red")
+  a = vector$first_arg
+  b = vector$second_arg
+  n = length(data)/3
+  redPoints = c()
+  bluePoints = c()
+  for(i in 1:n){
+    point = c(data[1+(3*(i-1))], data[2+(3*(i-1))])
+    if(data[3*i] == "red"){
+      redPoints = c(redPoints, point)
+    }
+    if(data[3*i] == "blue"){
+      bluePoints = c(bluePoints, point)
+    }
+  }
+  plot(printPoints(redPoints), col = "red")
   par(new=TRUE)
-  curve(a*x+b, xlim = c(-10,10), ylim = c(-10,10), xlab = "", ylab = "", col = "green")
+  #plot(printPoints(bluePoints), col = 'blue')
+  lines(curve(a*x+b), col="green")
 }
